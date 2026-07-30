aa56a28a99d8887546ba19b7bf154eb9edad590c
jihyeon-songb <dhqtl1@naver.com>
2026-07-31 04:45:57 +0900

    refactor: 컴포넌트와 스토리 구조 정리

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,3 +1,3 @@
-export { Button, type ButtonProps, type ButtonSize, type ButtonVariant } from "./Button.js"
-export { Input, type InputProps, type InputSize } from "./Input.js"
-export { Textarea, type TextareaProps, type TextareaSize } from "./Textarea.js"
+export { Button, type ButtonProps, type ButtonSize, type ButtonVariant } from "./actions/Button.js"
+export { Input, type InputProps, type InputSize } from "./inputs/Input.js"
+export { Textarea, type TextareaProps, type TextareaSize } from "./inputs/Textarea.js"
