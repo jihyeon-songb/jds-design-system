ae4cbe2d189efc2377581b4de07eab434144543e
jihyeon-songb <dhqtl1@naver.com>
2026-08-02 06:05:50 +0900

    fix: 버튼 인라인 로딩 크기 조정

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,9 +1,11 @@
 import type { StorybookConfig } from "@storybook/react-vite"
+import { mergeConfig } from "vite"
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.tsx"],
   addons: ["@storybook/addon-a11y"],
-  framework: "@storybook/react-vite"
+  framework: "@storybook/react-vite",
+  viteFinal: async (config) => mergeConfig(config, { optimizeDeps: { include: ["react", "react-dom"] } })
 }
 
 export default config
